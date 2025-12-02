87ac4493df9ac2366ee896e7ed09ead5e4dd8f62
Mark Schachner <markj.schachner@gmail.com>
2025-12-01 20:06:34 -0500

    Ok, actually finished day 1

--- day1/aoc25-1.js
+++ day1/aoc25-1.js
@@ -1,10 +1,11 @@
-/* Advent of Code 2025 Day 1: Safecracking
- * Task: given a file consisting of lines of the form "(R|L)N" where N is a
+/* Advent of Code 2025 Day 1: Safecracking */
+
+/* Part 1: given a file consisting of lines of the form "(R|L)N" where N is a
  * natural number, convert "RN" to N and "LN" to -N, then calculate how many
  * times the partial sum of the resulting integer sequence vanishes modulo 100.
  */
 
-import { readFile } from 'node:fs';
+import { readFileSync } from 'node:fs';
 
 // First the easy part: given an integer sequence, count how many
 // sums vanish modulo 100.
@@ -24,21 +25,53 @@ function zeroPartials(sequence) {
 // testSequence = [-68, -30, 48, -5, 60, -55, -1, -99, 14, -82];
 // console.log(zeroPartials(testSequence)); // returns 3
 
-// Next, read in the file and do the do.
+// Read in doc.txt and output the integer sequence.
 
-function crackSafe() {
-    readFile('doc.txt', 'utf8', (err, data) => {
-        let integerized = [];
-        let instructions = data.split("\n");
-        for (var i of instructions) {
-            let num = parseInt(i.slice(1));
-            let signedNum = i[0] == "R" ? num : -num;
-            integerized.push(signedNum);
-        }
-        console.log(zeroPartials(integerized));
-        return;
-    });
+function integerize() {
+    const data = readFileSync('doc.txt','utf8');
+    let instructions = data.split("\n");
+    let integerized = [];
+    for (var i of instructions) {
+        let num = parseInt(i.slice(1));
+        let signedNum = i[0] == "R" ? num : -num;
+        integerized.push(signedNum);
+    }
+    return integerized;
 }
 
-crackSafe();
+const instructions = integerize();
+
+// Put em together
+
+function crackSafe1() {
+    return zeroPartials(instructions);
+}
+
+ console.log(crackSafe1()); // returns 1129.
+
+
+
+/* Part 2: Now count how many times it sails past zero. */
+
+// Silliest possible way to do this: convert to clicks
+
+function toClicks(sequence) {
+    let clicks = [];
+    for (var entry of sequence) {
+        let newClicks = new Array(Math.abs(entry));
+        if (entry < 0) {
+            newClicks.fill(-1);
+        } else {
+            newClicks.fill(1);
+        }
+        clicks = clicks.concat(newClicks);
+    }
+    return clicks;
+}
+
+function crackSafe2() {
+    return zeroPartials(toClicks(instructions));
+}
+
+console.log(crackSafe2());
 
