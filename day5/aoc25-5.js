1b1fb0f9ed2de8f5c5d529298706eb520dd9ed61
Mark Schachner <markj.schachner@gmail.com>
2025-12-05 14:50:48 -0500

    Day 5 optimizing

--- day5/aoc25-5.js
+++ day5/aoc25-5.js
@@ -6,28 +6,12 @@
 
 import { readFileSync } from "fs";
 
-
-function parseRanges(rangeStrings) {
-    let rgs = [];
-    for (let rangeString of rangeStrings) {
-        let bounds = rangeString.split("-").map(n => parseInt(n));
-        rgs.push(bounds);
-    }
-    return rgs;
-}
-
-function inRange(id, rangeString) {
-    let bounds = rangeString.split("-");
-    bounds = bounds.map(n => parseInt(n));
-    return bounds[0] <= id && id <= bounds[1];
-}
-
 function readDatabase(fileName) {
     const lines = readFileSync(fileName,'utf8').split("\n");
     const divider = lines.indexOf("");
     return {
-        "rangeStrings": lines.toSpliced(divider),
-        "ids": lines.toSpliced(0,divider+1).map(n => parseInt(n))
+        "ranges": lines.toSpliced(divider).map(rStr => rStr.split("-").map(Number)),
+        "ids": lines.toSpliced(0,divider+1).map(Number)
     }
 }
 
@@ -35,8 +19,8 @@ function findFresh(fileName) {
     const data = readDatabase(fileName);
     let count = 0;
     for (let id of data.ids) {
-        for (let rangeString of data.rangeStrings) {
-            if (inRange(id,rangeString)) {
+        for (let range of data.ranges) {
+            if (range[0] <= id && id <= range[1]) {
                 count++;
                 break;
             }
@@ -46,29 +30,26 @@ function findFresh(fileName) {
 }
 
 //console.log(findFresh("input_test.txt")); // 3
-//console.log(findFresh("input.txt")); // 635
+console.log(findFresh("input.txt")); // 635
 
 /* Part 2: compute the size of a range.
  * Since the ranges are big, we're not going to brute force it. Rather, we'll
  * keep a list of ranges, and write a method for adding a new range.
  */
 
+
 // This function is disgusting. I don't know how to make it better.
 function addRange(ranges,newRange) {
     for (let range of ranges) {
-        if (newRange[1] < range[0] || range[1] < newRange[0]) {
-            continue;
-        }
-        if (range[0] <= newRange[0]) {
+        if (newRange[1] < range[0] || range[1] < newRange[0]) continue;
+        else if (range[0] <= newRange[0]) {
             if (range[1] < newRange[1]) {
                 newRange[0] = range[1]+1;
-                continue;
             }
             else return ranges;
         }
-        if (range[1] <= newRange[1]) {
+        else if (range[1] <= newRange[1]) {
             ranges.splice(ranges.indexOf(range),1);
-            continue;
         }
         else newRange[1] = range[0]-1;
     }
@@ -76,19 +57,13 @@ function addRange(ranges,newRange) {
     return ranges.push(newRange);
 }
 
-function collapseRanges(ranges) {
-    let collapsed = [];
-    for (let range of ranges) {
-        addRange(collapsed,range);
-    }
-    return collapsed;
-}
-const rangesLength = (ranges) => ranges.map(rg => rg[1]-rg[0]+1).reduce((a,b) => a+b);
-
 function countAllFreshes(fileName) {
-    const data = readDatabase(fileName);
-    return rangesLength(collapseRanges(parseRanges(data.rangeStrings)));
+    let collapsed = [];
+    readDatabase(fileName).ranges.forEach(
+        rg => addRange(collapsed,rg)
+    )
+    return collapsed.map(rg => rg[1]-rg[0]+1).reduce((a,b) => a+b);
 }
 
-console.log(countAllFreshes("input_test.txt")); // 14
+// console.log(countAllFreshes("input_test.txt")); // 14
 console.log(countAllFreshes("input.txt")); // 369761800782619
